683bd8fae939e83a9e51322c7ddc7b9e88296a83
Daniel azar <da6331@bs.amalnet.k12.il>
2024-09-30 09:14:53 +0300

    sd

--- src/Developer.java
+++ src/Developer.java
@@ -7,7 +7,7 @@ public class Developer extends Employee
 
     @Override
     public void getDetails(int salary) {
-        System.out.printf("Worker x working as a Developer gettig %d a month\n", salary);
+        System.out.printf("Worker x working as a Developer getting %d a month\n", salary);
     }
 
     public void performTask()
